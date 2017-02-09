95596d0c694de579ed86c282a59ab38738b701cf
森久 <hmori0217@gmail.com>
2017-02-09 19:51:38 +0900

    「MULTIPLE COMPONENTS」を作成

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -9,10 +10,9 @@ import { AppComponent }  from './app.component';
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
